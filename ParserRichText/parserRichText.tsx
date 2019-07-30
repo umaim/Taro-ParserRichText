57e4f4b824acb6602c32c2952b178ba224c84138
Cloud <9652227+xPixv@users.noreply.github.com>
2019-07-31 05:19:11 +0800

    feat: upgrade Parser
    - Update Parser (2019.7.24), https://github.com/jin-yufeng/Parser#%E6%9B%B4%E6%96%B0%E6%97%A5%E5%BF%97
    - Update README

--- ParserRichText/parserRichText.tsx
+++ ParserRichText/parserRichText.tsx
@@ -9,30 +9,42 @@ interface Props {
    * @default []
    */
   html: string | object | object[];
+
   /**
    * 是否允许链接受到点击时自动复制链接（仅限http(s)开头的网络链接）
    *
    * @default true
    */
   autocopy?: boolean;
+
   /**
    * 是否允许播放视频时自动暂停其他视频
    *
    * @default true
    */
   autopause?: boolean;
+
+  /**
+   * 是否自动将`<title>`标签的内容设置到页面标题上
+   *
+   * @default true
+   */
+  autosetTitle?: boolean;
+
   /**
    * 是否允许长按复制内容
    *
    * @default false
    */
   selectable?: boolean;
+
   /**
    * 标签的默认样式
    *
    * @default {}
    */
   tagStyle?: object;
+
   /**
    * 图片显示模式
    *
@@ -53,12 +65,14 @@ interface Props {
     | 'top right'
     | 'bottom left'
     | 'bottom right';
+
   /**
    * 是否使用渐显动画
    *
    * @default false
    */
   showWithAnimation?: boolean;
+
   /**
    * 渐显动画持续时间
    *
@@ -81,6 +95,7 @@ class ParserRichText extends Taro.Component<Props, {}> {
     html: [],
     autocopy: true,
     autopause: true,
+    autosetTitle: true,
     showWithAnimation: false,
     animationDuration: 400,
     selectable: false,
@@ -93,6 +108,7 @@ class ParserRichText extends Taro.Component<Props, {}> {
       html,
       autocopy,
       autopause,
+      autosetTitle,
       selectable,
       tagStyle,
       imgMode,
@@ -104,12 +120,13 @@ class ParserRichText extends Taro.Component<Props, {}> {
         html={html}
         autocopy={autocopy}
         autopause={autopause}
+        autosetTitle={autosetTitle}
         selectable={selectable}
         tag-style={tagStyle}
         img-mode={imgMode}
         show-with-animation={showWithAnimation}
         animation-duration={animationDuration}
-      ></parser>
+      />
     );
   }
 }
--- README.md
+++ README.md
@@ -8,14 +8,14 @@
 
 1. 将 `ParserRichText` 文件夹复制到项目 `components` 文件夹中
 2. 在需要富文本渲染的页面/组件中导入组件 `import ParserRichText from '组件路径';`
-3. 需要使用的地方使用 `ParserRichText` 组件即可
+3. 需要使用的地方使用 `ParserRichText` 组件即可，访问 [Parser](https://github.com/jin-yufeng/Parser) 了解参数详情及注意事项
 4. 在 [NervJS/taro#3885](https://github.com/NervJS/taro/issues/3885) 未解决之前，需要在 config/index.js 文件中添加 config.copy.patterns 复制组件到目标文件夹。参考：[config/index.js#L32](https://github.com/xPixv/SteamCN-Mini-Program/blob/f6ca35869f434127f9d88eb7db3977adb7fd1eb0/config/index.js#L32)
 
 **可访问 [Parser](https://github.com/jin-yufeng/Parser) 原仓库了解更多参数详解及注意事项**
 
 ## Demo 示例
 
-本组件用于 [SteamCN 论坛微信小程序](https://github.com/xPixv/SteamCN-Mini-Program)，可在此项目中参考使用方法及预览效果
+本组件用于 [SteamCN 论坛微信小程序](https://github.com/xPixv/SteamCN-Mini-Program) 主贴内容及回复内容的富文本渲染，可在此项目中参考使用方法及预览效果
 
 组件中使用：https://github.com/xPixv/SteamCN-Mini-Program/blob/master/src/components/ReplyCard/replyCard.tsx
 
